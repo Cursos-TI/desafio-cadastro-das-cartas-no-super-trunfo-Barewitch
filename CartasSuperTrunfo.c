899d89f214105e4d75fb8fc9823787e6fbc5e30f
Barewitch <jessica.smantovani@gmail.com>
2025-08-17 18:58:17 +0000

    Acrescentei o super poder de cada carta E fiz comparações, porém a o PIB per capita está com erro, farei ajustes mais tarde

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,6 +12,7 @@ int pontos1, pontos2;
 float densidade1, densidade2;
 float percapita1, percapita2;
 float divisao1, divisao2, divisao3, divisao4;
+float superpoder1, superpoder2;
 
     printf("Carta 1\n");
 
@@ -38,7 +39,9 @@ float divisao1, divisao2, divisao3, divisao4;
 
     divisao1 = (float) população1/area1;
 
-    divisao2 = (float) população1/pib1;
+    divisao2 = (float) pib1/população1;
+
+    superpoder1 = (float) população1 + pontos1 + pib1 + area1 + divisao1 + divisao2;
 
     printf("\n");
         
@@ -67,7 +70,9 @@ float divisao1, divisao2, divisao3, divisao4;
 
     divisao3 = (float) população2/area2;
 
-    divisao4 = (float) população2/pib2;
+    divisao4 = (float) pib2/população2;
+
+    superpoder2 = (float) população2 + pontos2 + pib2 + area2 + divisao3 + divisao4;
 
     printf("\n"); 
 
@@ -76,8 +81,9 @@ float divisao1, divisao2, divisao3, divisao4;
     printf("Carta 1\n");
     printf("Estado: %s \n Código da carta: %s \n Nome da cidade: %s \n", estado1, código1, nome1);
     printf("População: %d \n Pontos turísticos: %d \n PIB: %.2f bilhões \n Área: %.2f Km² \n", população1, pontos1, pib1, area1);
-    printf ("A densidade populacional é: %.2f hab/Km²\n", divisao1);
+    printf("A densidade populacional é: %.2f hab/Km²\n", divisao1);
     printf ("O PIB per capita  é: R$ %.2f \n", divisao2);
+    printf("Super Poder Carta 1 é: %.2f", superpoder1);
     printf("\n");
 
     printf("Carta 2\n");
@@ -85,6 +91,20 @@ float divisao1, divisao2, divisao3, divisao4;
     printf("População: %d \n Pontos turísticos: %d \n PIB: %.2f bilhões \n Área: %.2f Km² \n", população2, pontos2, pib2, area2);
     printf ("A densidade populacional é: %.2f hab/Km²\n", divisao3);
     printf ("O PIB per capita  é: R$ %.2f\n", divisao4);
+    printf("Super Poder Carta 2 é: %.2f\n", superpoder2);
+    printf("\n"); 
+
+    printf("Resultado \n");
+
+    printf("A população da Carta 1 (%d habitantes) é maior que o da Carta 2 (%d habitantes): %d\n", população1, população2, população1 > população2);
+    printf("A quantidade de pontos turísticos da Carta 1 (%d) é maior que o da Carta 2 (%d): %d\n", pontos1, pontos2, pontos1 > pontos2);
+    printf("O PIB da Carta 1 (%.2f bilhões) é maior que o da Carta 2 (%.2f bilhões): %d\n", pib1, pib2, (float) pib1 > pib2);
+    printf("A área da Carta 1 (%.2f km²) é maior que o da Carta 2 (%.2f km²): %d\n", area1, area2, (float) area1 > area2);
+    printf("A densidade populacional da Carta 1 (%.2f hab/km²) é maior que o da Carta 2 (%.2f hab/km²): %d\n", população1 / area1, população2 / area2, (float) (população1 / area1) > (população2 / area2));
+    printf("O PIB per capita da Carta 1 (R$ %.2f reais) é maior que o da Carta 2 (R$ %.2f): %d\n", divisao2, divisao4, pib1 / população1, pib2 / população2, (float) (pib1 / população1) > ( pib2 / população2));
+    printf("O Super Poder da Carta 1 (%.2f) é maior que o da Carta 2 (%.2f): %d\n", superpoder1, superpoder2, (float) superpoder1 > superpoder2);
+
+  
 
     return 0;
 
